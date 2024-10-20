83737f94ced3609b5fdccb0e934b7b0453b800b7
AlexKalll <161981213+AlexKalll@users.noreply.github.com>
2024-10-20 20:32:09 +0300

    Create 1106. Parsing A Boolean Expression.py
    https://leetcode.com/problems/parsing-a-boolean-expression

---
+++ 1106. Parsing A Boolean Expression.py
@@ -0,0 +1,37 @@
+class Solution:
+    def parseBoolExpr(self, expression: str) -> bool:
+        expression=expression.replace(',','')
+        stack=[]
+        def is_op(c):
+            return (c=='|' or c=='&' or c=='!')
+        def perform_and(l):
+            return False not in l
+        def perform_or(l):
+            return True in l 
+
+        for i in expression:
+            if(i=='('):
+                stack.append(i)
+            elif(is_op(i)):
+                stack.append(i)
+            elif(i=='f'):
+                stack.append(False)
+            elif(i=='t'):
+                stack.append(True)
+            elif(i==')'):
+                l=[]
+                while(stack[-1]!='('):
+                    l.append(stack.pop())
+                stack.pop()
+                op = stack.pop()
+                if(op=='&'):
+                    stack.append(perform_and(l))
+                elif(op =='|'):
+                    stack.append(perform_or(l))
+                else:
+                    stack.append(not l[0])
+        return (stack[0])
+                    
+                    
+                    
+                
