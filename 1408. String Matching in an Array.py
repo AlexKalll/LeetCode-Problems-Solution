7f6f5a51ba1a86f524bcd9570f16b7f07531f118
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-01-08 18:47:40 +0300

    1408. String Matching in an Array.py
    https://leetcode.com/problems/string-matching-in-an-array/

---
+++ 1408. String Matching in an Array.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/string-matching-in-an-array/
+
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        result = []
+        n = len(words)
+        for i in range(n):
+            for j in range(n):
+                if i != j and words[i] in words[j]:
+                    result.append(words[i])
+                    break
+        return result
