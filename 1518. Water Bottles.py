e4c204c2bd56e4bdc543efa2cf852aa54df9836f
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-02-03 18:57:24 +0300

    1518. Water Bottles.py

---
+++ 1518. Water Bottles.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/water-bottles/description/
+
+class Solution:
+    def numWaterBottles(self, n: int, m: int) -> int:
+        # n = numBottles   and   m = numExchange
+        ans = n
+        
+        while n // m > 0:
+            ans += (n//m)
+            n = (n//m) + (n%m)
+
+        return ans 
