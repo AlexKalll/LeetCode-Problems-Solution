fd079f8132497cc231116c329a5ec2f7399a7191
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-02-06 20:36:31 +0300

    Time: 0 ms (100%), Space: 17.7 MB (76.88%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,7 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        result = []
+        while matrix:
+            result += matrix.pop(0)  # pop the first row each time
+            matrix = list(zip(*matrix))[::-1] # left rotate matrix 90 degree each time
+        return result
