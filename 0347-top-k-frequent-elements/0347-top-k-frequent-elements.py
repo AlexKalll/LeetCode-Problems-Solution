370404871170a56ca3cba0b6f497cc7bcc4a0a0b
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-02-07 09:58:55 +0300

    Time: 0 ms (100%), Space: 21.4 MB (39.1%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,17 +2,7 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         nums_count = Counter(nums)
         # sort the dictionary based one the values in reverse order and return a list
-        hT = sorted(nums_count.items(), key = lambda item: item[1], reverse = True)
-        # changing it to a dictionary
-        hT = dict(hT)
-
-        ans = []
-        i = 0
-        for key in hT:
-            if i >= k:
-                break 
-
-            ans.append(key)
-            i += 1
+        hT = nums_count.most_common(k)
+        ans = [key for key, val in hT]
         
-        return ans 
+        return ans
