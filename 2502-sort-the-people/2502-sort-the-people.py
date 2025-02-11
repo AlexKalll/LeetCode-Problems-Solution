4dd68731f9a5968996a28b1e64657f021e79e340
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-02-11 09:56:39 +0300

    Time: 296 ms (9.32%), Space: 18.1 MB (66.07%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -1,6 +1,7 @@
+"""
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        
+        #using bubble sort 
         n = len(heights)
 
         for i in range(n):
@@ -16,3 +17,51 @@ class Solution:
                 break
                 
         return names
+"""
+"""
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        # using selection sort 
+        n = len(heights)
+
+        for i in range(n):
+            smallest = i
+
+            for j in range(i+1, n):
+                if heights[j] > heights[smallest]:
+                    smallest = j
+
+            heights[smallest], heights[i] = heights[i], heights[smallest]
+            names[smallest], names[i] = names[i], names[smallest]
+
+        return names
+"""
+
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        n = len(heights)
+
+        for i in range(1, n):
+            key_height = heights[i]
+            key_name = names[i]
+            j = i - 1
+
+            # Move elements of heights and names that are less than key_height
+            while j >= 0 and key_height > heights[j]:
+                heights[j + 1] = heights[j]
+                names[j + 1] = names[j]
+                j -= 1
+            
+            
+            heights[j + 1] = key_height
+            names[j + 1] = key_name
+        
+        return names
+
+
+
+        # j = i-1
+        # while j >= 0 and arr[i+1]<arr[j]:
+        #     arr[j+1] = arr[j]
+        #     j -= 1
+            
