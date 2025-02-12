9dc7ea4f2d553d73f5170b097a65f9331f19dc70
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-02-12 19:21:44 +0300

    Time: 78 ms (59.55%), Space: 29.3 MB (22.42%) - LeetHub

---
+++ 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        
+        M = max(costs)
+        m = min(costs) # offset
+        width = M - m + 1
+        
+        count = [0] * width 
+
+        for cost in costs:
+            count[cost-m] += 1
+
+        creams = 0
+
+        for i in range(len(count)):
+            if coins >= count[i] * (i + m) and count[i] != 0:    
+                creams += count[i]
+                coins -= count[i] * (i+m)
+
+            else:
+                while coins >= (i + m) and count[i] != 0:
+                    creams += 1
+                    coins -= (i + m)
+                    count[i] -= 1
+        
+        return creams 
