65ace6e52d26f466330163fa92ebc0f3cf919770
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-02-13 17:08:21 +0300

    Time: 35 ms (82.12%), Space: 18.1 MB (37.9%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -5,8 +5,8 @@ class Solution:
 
         while left < right:
             if s[left] != s[right]:
-                s1 = s[:left] + s[left+1:]
-                s2 = s[:right] + s[right+1:]
+                s1 = s[left:right] # the case for if s[left] == s[right-1]
+                s2 = s[left+1:right+1] # if s[left+1] == s[right]
 
                 return s1 == s1[::-1] or s2 == s2[::-1]
             
