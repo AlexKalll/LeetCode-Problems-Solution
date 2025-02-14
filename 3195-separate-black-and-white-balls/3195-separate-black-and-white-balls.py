1ddb2184533bca5158c01fc555c50d9ce5b69ce1
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-02-14 15:17:48 +0300

    Time: 40 ms (92.81%), Space: 18.2 MB (29.39%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.py
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.py
@@ -1,12 +1,13 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
-        num_ones = 0
+
+        num_ones = 0 # to count the # of ones before the current zero
         ans = 0
 
         for binary in s:
-            if binary == "1":
+            if binary == "1": 
                 num_ones += 1
             else:
-                ans += num_ones
+                ans += num_ones # No- of flips required to take the currnet zero to a position before all ones
         
         return ans 
