5103b1a676fdecb40851313df1d5a62873ec2620
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-02-18 15:36:18 +0300

    Time: 448 ms (43.53%), Space: 33.5 MB (76.88%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -1,21 +1,20 @@
 class Solution:
     def maximumSum(self, nums: List[int]) -> int:
-        ht = defaultdict(list) # a hash map to  store two numbers that their digit sum is equal and these should be tha largests among all possibles
-
+        
+        nums.sort()
+        ht = defaultdict(list) 
+        
         for num in nums:
-            curr = sum(list(map(int, str(num))))
-            if curr not in ht:
-                ht[curr] = [0, 0]
-            
-            if num > ht[curr][1]: 
-                ht[curr][1] = num
-                if ht[curr][1] > ht[curr][0]:
-                    ht[curr][0], ht[curr][1] = ht[curr][1], ht[curr][0]
+            curr_sum = sum(map(int, list(str(num))))
+            ht[curr_sum].append(num)
+
         ans = 0
-        print(ht)
-        for curr in ht:
-            if ht[curr][0] > 0 and ht[curr][1] > 0:
-                ans = max(ans, sum(ht[curr]))
+
+        for key in ht:
+            if len(ht[key]) >= 2:
+                curr_ans = ht[key].pop() + ht[key].pop()
+                ans = max(ans, curr_ans)
+        
         if ans:
             return ans 
         
