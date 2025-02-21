a3f232242ce783d1cbabef7301d517c194fb8190
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-02-21 11:06:14 +0300

    Time: 0 ms (100%), Space: 22.4 MB (10.7%) - LeetHub

--- 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.py
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.py
@@ -5,6 +5,43 @@
 #         self.left = left
 #         self.right = right
 class FindElements:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.root = root
+        self.recovered_nodes = set()
+        self.q = deque([])
+        
+        self.bfs(self.root, 0)
+    # using bfs 
+    def bfs(self, node, val):
+        if not node:
+            return 
+        self.recovered_nodes.add(0) # initially 0 since it is guaranteed 
+        node.val = val
+        self.q.append(node)
+
+        while self.q:
+            node = self.q.popleft()
+            curr_val = node.val
+            if node.left:
+                self.q.append(node.left)
+                node.left.val = 2*curr_val + 1
+                self.recovered_nodes.add(node.left.val)
+            if node.right:
+                self.q.append(node.right)
+                node.right.val = 2*curr_val + 2
+                self.recovered_nodes.add(node.right.val)
+
+    def find(self, target: int) -> bool:
+        
+        return target in self.recovered_nodes
+
+# Your FindElements object will be instantiated and called as such:
+# obj = FindElements(root)
+# param_1 = obj.find(target)
+
+"""
+class FindElements:
     def __init__(self, root: Optional[TreeNode]):
         self.root = root 
         self.node_values = set()
@@ -22,6 +59,4 @@ class FindElements:
         
         return (target in self.node_values)
 
-# Your FindElements object will be instantiated and called as such:
-# obj = FindElements(root)
-# param_1 = obj.find(target)
+"""
