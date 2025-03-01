0170e4ec760a048f496048806cce1bc6944324df
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-01 14:26:09 +0300

    Time: 0 ms (100%), Space: 17.7 MB (94.08%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
@@ -1,17 +1,18 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        # applying the operations 
-        for i in range(len(nums)-1):
-            if nums[i] == nums[i+1] and nums[i] != 0:
+        n = len(nums)
+
+        for i in range(n-1):
+            if nums[i] == nums[i+1]:
                 nums[i] *= 2
                 nums[i+1] = 0
 
-        # applying two pointers approach to push all zeros to the end
-        l  = 0
-        print(nums)
-        for r in range(len(nums)):
-            if nums[r] != 0:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
+        holder, seeker = 0, 0
+        while seeker < n:
+            if nums[seeker] != 0:
+                nums[seeker], nums[holder] = nums[holder], nums[seeker]
+                holder += 1
+            
+            seeker += 1
 
         return nums
