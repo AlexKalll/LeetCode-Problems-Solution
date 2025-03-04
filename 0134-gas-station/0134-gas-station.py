c95a2914f0d2a59bc04107be8aeee07fb5a25002
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-04 21:52:12 +0300

    Time: 39 ms (23.46%), Space: 23.3 MB (83.48%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total_gas = 0
+        total_cost = 0
+        curr_tank = 0
+        start_index = 0
+        
+        for i in range(len(gas)):
+            total_gas += gas[i]
+            total_cost += cost[i]
+            curr_tank += gas[i] - cost[i]
+            
+            if curr_tank < 0:
+                start_index = i + 1  
+                curr_tank = 0  
+        
+        
+        if total_gas < total_cost:
+            return -1
+        
+        return start_index
