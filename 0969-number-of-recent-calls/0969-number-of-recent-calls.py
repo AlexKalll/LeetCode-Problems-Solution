c3033a385209364625c308ac9ee961627b94db78
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-05 19:17:15 +0300

    Time: 45 ms (45.33%), Space: 23.2 MB (22.02%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.py
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.py
@@ -1,19 +1,16 @@
 class RecentCounter:
 
     def __init__(self):
-        self.count = 0
         self.q = []
         self.i = 0
 
     def ping(self, t: int) -> int:
-        
         self.q.append(t)
-        self.count += 1
         
-        while self.q[self.count-1] - self.q[self.i] > 3000:
+        while self.q[len(self.q)-1] - self.q[self.i] > 3000:
             self.i += 1
         
-        return self.count - self.i
+        return len(self.q) - self.i
 
 
         
