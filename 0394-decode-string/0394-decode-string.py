71e919b0b0e59b06a3a6b35f4fe0bb18fb373d15
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-14 17:39:50 +0300

    Time: 0 ms (100%), Space: 17.8 MB (55.38%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,4 +1,4 @@
-class Solution:
+"""class Solution:
     def decodeString(self, s: str) -> str:
         stack = []
 
@@ -18,3 +18,38 @@ class Solution:
                 stack.append(curr * int(x))
 
         return ''.join(stack)
+
+"""
+class Solution:
+    def decodeString(self, s: str) -> str:
+        def decode(index):
+            result = ''
+            num = 0
+            
+            while index < len(s):
+                char = s[index]
+                
+                if char.isdigit():
+                    # Build the number (could be more than one digit)
+                    num = num * 10 + int(char)
+                
+                elif char == '[':
+                    # Recursively decode the substring
+                    index, decoded_string = decode(index + 1)
+                    result += decoded_string * num  # Repeat the decoded string num times
+                    num = 0  # Reset num after using it
+                
+                elif char == ']':
+                    # Return the current result and the next index
+                    return index, result
+                
+                else:
+                    # Normal character, just add to the result
+                    result += char
+                
+                index += 1
+            
+            return result
+        
+        # Start decoding from index 0
+        return decode(0)
