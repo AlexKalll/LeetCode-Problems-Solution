344f92c7aeca7db38bf30449356a210fe3c483d7
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-17 18:38:11 +0300

    Time: 0 ms (100%), Space: 19.4 MB (43.38%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.py
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.py
@@ -4,17 +4,38 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+"""
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        def helper(node, val):
+
+        def dfs(node, val):
             if not node:
                 return None
+                
             if val == node.val:
                 return node
+
             elif val < node.val:
-                return helper(node.left, val)
+                return dfs(node.left, val)
             else:
-                return helper(node.right, val)
+                return dfs(node.right, val)
         
         return helper(root, val)
             
+"""
+# BFS approach 
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+
+        q = deque([root])
+        while q:
+            node = q.popleft()
+            if node.val == val:
+                return node
+            
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+            
+        return None
