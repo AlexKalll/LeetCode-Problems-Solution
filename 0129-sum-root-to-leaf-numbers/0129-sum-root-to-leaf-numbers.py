a132aae8911e3867400068b03cbb91c01a59a245
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-21 18:23:50 +0300

    Time: 0 ms (100%), Space: 17.7 MB (86.5%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -4,31 +4,48 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
+"""class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        
-        def dfs(node, curr):
-            nonlocal ans
-            if not node:
-                return 
-            
-            # curr = curr * 10 + node.val
+
+        curr = root.val
+        def dfs(node):
+            nonlocal ans, curr
 
             if not node.left and not node.right:
                 ans += curr
                 return 
-
+            # using backtracking concept
             arr = [node.left, node.right]
             for child in arr:
                 if not child:
                     continue
                 curr = curr * 10
                 curr += child.val
-                dfs(child, curr)
+                dfs(child)
                 curr -= child.val
                 curr //= 10
 
         ans = 0
-        dfs(root, root.val)
+        dfs(root)
         return ans
             
+"""
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+
+        def dfs(node, curr):
+            nonlocal ans
+            if not node:
+                return 
+
+            curr = curr * 10 + node.val
+            if not node.left and not node.right:
+                ans += curr
+                return 
+            
+            dfs(node.left, curr)
+            dfs(node.right, curr)
+
+        ans = 0
+        dfs(root, 0)
+        return ans
