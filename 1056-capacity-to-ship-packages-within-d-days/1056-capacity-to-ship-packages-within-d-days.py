3435afa17f13e745d685f6cb9fdcd73f8c2c30cd
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-25 19:51:16 +0300

    Time: 183 ms (67.29%), Space: 21.8 MB (99.45%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,29 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+
+        def isPossible(capacity):
+            count = 1
+            curr_sum = 0
+            for num in weights:
+                if curr_sum + num <= capacity:
+                    curr_sum += num
+                else:
+                    curr_sum = num
+                    count += 1
+
+            return count <= days
+
+        low = max(weights)
+        high = sum(weights)
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if isPossible(mid):
+                least_cap = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return least_cap
+            
