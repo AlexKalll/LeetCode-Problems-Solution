f83119abc155acd4c6676315b966dad9b3e25f88
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-25 19:19:59 +0300

    Time: 32 ms (87.51%), Space: 17.6 MB (68.08%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -3,6 +3,20 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
+        
+        left =  1
+        right = n
+        while left <= right:
+            mid = (left + right) //  2
+            
+            if isBadVersion(mid):
+                right = mid -1
+            else:
+                left = mid + 1
+            
+        return left
+        
+"""
         left =  1
         right = n
 
@@ -16,3 +30,4 @@ class Solution:
                 left = mid + 1
             
         return first_bad
+"""
