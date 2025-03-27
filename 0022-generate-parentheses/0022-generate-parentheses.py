6b55261a57f014f6f24bd6924922cc2da4c5c99e
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-27 19:26:45 +0300

    Time: 2 ms (35.51%), Space: 18 MB (69.94%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,4 +1,4 @@
-class Solution:
+"""class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         ans = []
         def backtrack(curr, open_count, close_count):
@@ -18,3 +18,28 @@ class Solution:
         
         backtrack([], 0, 0)
         return ans 
+    """
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+        def backtrack(curr, open_count, close_count):
+            if len(curr) == 2*n:
+                ans.append(''.join(curr))
+                return 
+        
+            for par in '()':
+                if par == '(' and open_count >= n:
+                    continue
+                if par == ')' and close_count >= open_count:
+                    continue
+                
+                curr.append(par)
+                if par == '(':
+                    backtrack(curr, open_count + 1, close_count)
+                if par == ')':
+                    backtrack(curr, open_count , close_count+ 1)
+            
+                curr.pop()
+
+        backtrack([], 0, 0)
+        return ans
