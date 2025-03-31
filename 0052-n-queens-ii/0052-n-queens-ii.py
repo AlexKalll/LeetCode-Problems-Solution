280aec1727257e5490f2b1321d10ccc5b2d9e8b1
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-31 21:58:40 +0300

    Time: 40 ms (12.47%), Space: 18.1 MB (8.74%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,36 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        ans = []
+        
+        # check if possible to place Q in (row, col)
+        def can_draw(row, col):
+            for i in range(row):
+                # check for the column
+                if board[i][col] == 'Q':
+                    return False
+                # check for the diagonals
+                if col - (row - i) >= 0 and board[i][col - (row - i)] == 'Q':
+                    return False
+                if col + (row - i) < n and board[i][col + (row - i)] == 'Q':
+                    return False
+
+            return True
+
+        def backtrack(row):
+            if row == n:
+                ans.append([''.join(curr_row) for curr_row in board])
+                return
+            
+            for col in range(n):
+                if not can_draw(row, col):
+                    continue
+                    
+                board[row][col] = 'Q'
+                backtrack(row + 1)      
+                board[row][col] = '.'
+
+        board = [['.' for _ in range(n)] for _ in range(n)]
+        backtrack(0)
+
+        return len(ans)
+
