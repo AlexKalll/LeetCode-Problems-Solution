017c4634e5eaed734b41de810d7d0a97ad659d6f
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-04-08 18:22:43 +0300

    Time: 607 ms (44.13%), Space: 139.8 MB (34.36%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
@@ -9,13 +9,13 @@ class Solution:
 
         # print(graph)
         visited = set()
-        
-        def dfs(src):
-            if src == destination:
+
+        def dfs(curr_node):
+            if curr_node == destination:
                 return True
 
-            visited.add(src)
-            for nbr in graph[src]:
+            visited.add(curr_node)
+            for nbr in graph[curr_node]:
                 if nbr not in visited:
                     if dfs(nbr):
                         return True
