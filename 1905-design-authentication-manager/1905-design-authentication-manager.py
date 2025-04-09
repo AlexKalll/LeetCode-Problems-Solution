99f32593b67c883c2ec1acb8342b67e8665380ec
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-04-09 22:06:14 +0300

    Time: 14 ms (86.67%), Space: 19.4 MB (55.62%) - LeetHub

---
+++ 1905-design-authentication-manager/1905-design-authentication-manager.py
@@ -0,0 +1,54 @@
+class ListNode():
+    def __init__(self, tokenId='', expiryTime=0):
+        self.tokenId, self.expiryTime = tokenId, expiryTime
+        self.next = self.prev = None
+
+class AuthenticationManager:
+
+    def __init__(self, timeToLive: int):
+        self.authManager = dict()  # tokenId -> reference to node
+        self.start, self.end = ListNode(), ListNode()
+        self.start.next, self.end.prev = self.end, self.start  # Link dummy nodes
+        self.size = 0
+        self.ttl = timeToLive
+
+    def insert(self, node):
+        prev, end = self.end.prev, self.end
+        prev.next, end.prev = node, node
+        node.prev, node.next = prev, end
+        self.size += 1
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+        self.size -= 1
+        
+    def generate(self, tokenId: str, currentTime: int) -> None:
+        expiry = currentTime + self.ttl
+        self.authManager[tokenId] = ListNode(tokenId, expiry)
+        self.insert(self.authManager[tokenId])
+
+    def renew(self, tokenId: str, currentTime: int) -> None:
+        if tokenId in self.authManager:
+            node = self.authManager[tokenId]
+            if node.expiryTime > currentTime:
+                # Remove node from current position
+                self.remove(node)
+                # Update expiry time
+                node.expiryTime = currentTime + self.ttl
+                # Reinsert at the end
+                self.insert(node)
+            else:
+                # If expired, remove from map
+                self.remove(node)
+                del self.authManager[tokenId]
+
+    def countUnexpiredTokens(self, currentTime: int) -> int:
+        start = self.start.next
+
+        while start != self.end and start.expiryTime <= currentTime:
+            self.remove(start)
+            del self.authManager[start.tokenId]  # Also remove from dictionary
+            start = start.next  # Move to the next node
+        
+        return self.size
