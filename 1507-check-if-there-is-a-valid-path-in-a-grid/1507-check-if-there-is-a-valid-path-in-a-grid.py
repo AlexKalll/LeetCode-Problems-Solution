3fd60b8ba28b5128fc65336f6e52344f80081552
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-05-12 18:25:01 +0300

    Time: 795 ms (19.44%), Space: 25.5 MB (85.65%) - LeetHub

--- 1507-check-if-there-is-a-valid-path-in-a-grid/1507-check-if-there-is-a-valid-path-in-a-grid.py
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/1507-check-if-there-is-a-valid-path-in-a-grid.py
@@ -1,22 +1,38 @@
+class UnionFind:
+    def __init__(self, size):
+        self.parent = list(range(size))
+        self.rank = [1] * size
+
+    def find(self, u):
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def union(self, u, v):
+        root_u = self.find(u)
+        root_v = self.find(v)
+        if root_u != root_v:
+            if self.rank[root_u] > self.rank[root_v]:
+                self.parent[root_v] = root_u
+            elif self.rank[root_u] < self.rank[root_v]:
+                self.parent[root_u] = root_v
+            else:
+                self.parent[root_v] = root_u
+                self.rank[root_u] += 1
+
 class Solution:
     def hasValidPath(self, grid: List[List[int]]) -> bool:
         rows, cols = len(grid), len(grid[0])
-        drs = {1:[(0,-1),(0,1)],2:[(1,0),(-1,0)],3:[(0,-1),(1,0)],
-                    4:[(0,1),(1,0)],5:[(0,-1),(-1,0)],6:[(0,1),(-1,0)]}
-        q = deque()
-        visited = set()
-        q.append((0,0))
-        visited.add((0,0))
-        
-        while q:
-            x,y=q.popleft()
-            if x == rows-1 and y == cols-1:
-                return True
-            for dx,dy in drs[grid[x][y]]:
-                next_x = x + dx
-                next_y = y + dy
-                if 0 <= next_x < rows and 0 <= next_y < cols and (next_x,next_y) not in visited:
-                    if (-dx,-dy) in drs[grid[next_x][next_y]]:
-                        q.append((next_x,next_y))
-                        visited.add((next_x,next_y))
-        return False
+        drs = {1: [(0, -1), (0, 1)], 2: [(1, 0), (-1, 0)], 3: [(0, -1), (1, 0)],
+               4: [(0, 1), (1, 0)], 5: [(0, -1), (-1, 0)], 6: [(0, 1), (-1, 0)]}
+        dsu = UnionFind(rows * cols)
+
+        for r in range(rows):
+            for c in range(cols):
+                for dx, dy in drs[grid[r][c]]:
+                    nr, nc = r + dx, c + dy
+                    if 0 <= nr < rows and 0 <= nc < cols:
+                        if (-dx, -dy) in drs[grid[nr][nc]]:
+                            dsu.union(r * cols + c, nr * cols + nc)
+
+        return dsu.find(0) == dsu.find((rows - 1) * cols + (cols - 1))
