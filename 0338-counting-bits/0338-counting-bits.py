e879c3e77e3cc92e73d11c4e572fa3c10ba11a7f
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-05-13 20:00:14 +0300

    Time: 3 ms (94.08%), Space: 18.5 MB (90.39%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,5 +1,6 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        """
         ans = []
         
         for num in range(n+1):
@@ -10,4 +11,10 @@ class Solution:
                 num >>= 1  # num = num // 2
             ans.append(count)
 
+        return ans
+        """
+        ans = [0] * (n+1)
+        for i in range(1, n+1):
+            ans[i] = ans[i>>1] + (i & 1) # ans[i] = ans[i//2] + (i % 2)
+        
         return ans
