74112cafd4664dacb9e2b175c073d8179fbbd8d9
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-06-06 21:25:22 +0300

    Time: 123 ms (46.13%), Space: 19.1 MB (45.28%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.py
@@ -0,0 +1,21 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        d = {j.id:i for i, j in enumerate(employees)}
+        s = [id] 
+        ans = 0
+
+        while(s):
+            curr = s.pop()
+            ans += employees[d[curr]].importance
+            for child in employees[d[curr]].subordinates:             
+                s.append(child)
+        return ans
