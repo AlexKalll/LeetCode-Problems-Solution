69a1874273049a75d91215ab1d24f375039a4313
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-06-10 18:51:59 +0300

    Time: 2 ms (88.78%), Space: 18.6 MB (68.16%) - LeetHub

---
+++ 0800-letter-case-permutation/0800-letter-case-permutation.py
@@ -0,0 +1,15 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        output=[""]
+        for i in s:
+            tmp=[]
+            if i.isalpha():
+                for j in output:
+                    tmp.append(j+i.lower())
+                    tmp.append(j+i.upper())
+            else:
+                for j in output:
+                    tmp.append(j+i)
+            output=tmp
+        return output
+        
