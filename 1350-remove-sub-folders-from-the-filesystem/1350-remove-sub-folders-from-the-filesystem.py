e97c8ce68ce8f1b18302c1fb0276ef8ea5cd6774
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-07-20 20:31:32 +0300

    Time: 21 ms (88.7%), Space: 30.6 MB (77.26%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.py
@@ -8,7 +8,7 @@ class Solution:
         
         # Iterate through the sorted folder list
         for f in folder:
-            # If result is empty or the current folder is not a subfolder of the last one in result
+            
             if not result or not f.startswith(result[-1] + '/'):
                 result.append(f)
         
