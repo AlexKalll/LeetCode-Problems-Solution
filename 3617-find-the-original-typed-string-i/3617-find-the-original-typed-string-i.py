35e46ed163cd01864fb5d0d01513a90223002efd
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-08-20 23:26:54 +0300

    Time: 51 ms (5.34%), Space: 17.7 MB (70.72%) - LeetHub

---
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.py
@@ -0,0 +1,14 @@
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        res = 1
+        i = 0
+        l = len(word)
+        while i < l:
+            val = 1
+            cur = word[i]
+            while i < l - 1 and cur == word[i + 1]:
+                val += 1
+                i += 1
+            i += 1
+            res += val - 1
+        return res
