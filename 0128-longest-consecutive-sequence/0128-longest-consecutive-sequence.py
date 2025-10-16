f126efa5a2da311a8c29860d9004c683f67a81c5
Kaletsidik Ayalew <alexkalalw@gmail.com>
2025-10-16 11:07:30 +0300

    Time: 46 ms (76.47%), Space: 33.2 MB (89.16%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,33 +1,12 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        root = {}
-        size = {}
-
-        # Find with path compression
-        def find(x):
-            if root[x] != x:
-                root[x] = find(root[x])
-            return root[x]
-
-        # Union by size
-        def union(x, y):
-            rootX, rootY = find(x), find(y)
-            if rootX != rootY:
-                if size[rootX] > size[rootY]:
-                    root[rootY] = rootX
-                    size[rootX] += size[rootY]
-                else:
-                    root[rootX] = rootY
-                    size[rootY] += size[rootX]
-
-        for num in nums:
-            root[num] = num
-            size[num] = 1
-
-        for num in nums:
-            if num + 1 in root:
-                union(num, num + 1)
-
-        return max(size.values())
+        s=set(nums)
+        m=0
+        for n in s:
+            if n-1 not in s:
+                l=1
+                while n+1 in s:
+                    n+=1
+                    l+=1
+                m=max(m,l)
+        return m
