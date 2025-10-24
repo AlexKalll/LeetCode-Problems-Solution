4cfad0bf9054dbc7d2533c50326f1c45a303f385
Kaletsidik Ayalew <alexkalalw@gmail.com>
2025-10-24 21:05:53 +0300

    Time: 3277 ms (29.65%), Space: 17.9 MB (26.16%) - LeetHub

---
+++ 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.py
@@ -0,0 +1,19 @@
+from collections import Counter
+class Solution:
+    def nextBeautifulNumber(self, n: int) -> int:
+        def check(n):
+            mpp = Counter(str(n))
+            for n, f in mpp.items():
+                if str(f) != n:
+                    return False
+
+            return True
+
+        i = n+1
+        while True:
+            if check(i):
+                return i
+
+            i += 1
+            
+        return 0
