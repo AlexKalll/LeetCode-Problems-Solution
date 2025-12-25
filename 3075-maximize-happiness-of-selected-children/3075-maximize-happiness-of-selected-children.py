2afcf93d89d74d2c25a89d24a75279f149fe7918
Kaletsidik Ayalew <alexkalalw@gmail.com>
2025-12-25 18:50:18 +0300

    Time: 824 ms (58.87%), Space: 43.2 MB (97.16%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness.sort(reverse = True)
+        ans = 0
+        i = 0
+
+        for i in range(k):
+            ans += (happiness[i] - i if happiness[i]-i > 0 else 0)
+            i += 1
+        
+        return ans
