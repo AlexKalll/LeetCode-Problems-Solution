2a83382f4abb1b1990b71c58c02365f45e4cd37f
Kaletsidik Ayalew <alexkalalw@gmail.com>
2026-02-05 21:15:28 +0300

    Time: 56 ms (74.44%), Space: 19.4 MB (34.53%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        result = []
+        n = len(nums)
+        for i in range(n):
+            if nums[i] > 0:
+                landing = (i + nums[i]) % n
+                result.append(nums[landing])
+            elif nums[i] < 0:
+                landing = (i + nums[i]) % n
+                result.append(nums[landing])      
+            else: 
+                result.append(nums[i])
+            
+        return result
