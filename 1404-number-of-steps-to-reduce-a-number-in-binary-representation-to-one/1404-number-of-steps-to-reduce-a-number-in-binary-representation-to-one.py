5b5ec4b79acd7a3dbd3bb4539876a0bfb4236147
Kaletsidik Ayalew <alexkalalw@gmail.com>
2026-02-27 22:04:42 +0300

    Time: 0 ms (100%), Space: 19.1 MB (87.5%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -1,19 +1,12 @@
 class Solution:
     def numSteps(self, s: str) -> int:
-        # Convert binary string to a decimal integer
-        num = int(s, 2)
-        steps = 0
 
-        while num != 1:
-            # If even, divide by 2
-            if num % 2 == 0:
-                num = num // 2
-                steps += 1
-            else:
-            #If odd, add 1 (making it even) then divide by 2
-            # This counts as 2 steps
-                num += 1
-                num = num // 2
-                steps += 2
-
-        return steps
+        n, tally = int(s,2), 0
+        
+        while n != 1: 
+            if n&1: n+= 1 
+            else:   n>>= 1    
+            tally+= 1
+        
+        return tally
+        
