cb17d8ecd83e62a471032bd5b8e3d2a6a518217b
Kaletsidik Ayalew <alexkalalw@gmail.com>
2026-07-23 20:33:01 +0300

    Time: 182 ms (6.95%), Space: 140.5 MB (25.96%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        int n = s.size();
-        int ones = 0;
-        for (char ch : s) {
-            if (ch == '1') ones++;
+        int n=s.size();
+        vector<int> left(n),right(n);
+        int cnt=0;
+        for(int i=0;i<n;i++){
+           if(s[i]=='0'){
+            cnt++;
+            left[i]=cnt;
+           }
+           else {
+            left[i]=cnt;
+            cnt=0;
+           }
         }
-
-        int l = 0, r = n - 1;
-        while (l < n && s[l] == '1') l++;
-        while (r >= 0 && s[r] == '1') r--;
-
-        if (l > r) return ones;
-
-        vector<pair<int, char>> store;
-        int len = 1;
-        for (int i = l + 1; i <= r; i++) {
-            if (s[i] == s[i - 1]) {
-                len++;
+        cnt=0;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='0'){
+            cnt++;
+            right[i]=cnt;
+           }
+           else {
+            right[i]=cnt;
+            cnt=0;
+           }
+        }
+        cnt=0;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='0') continue;
+            int j=i;
+            while(j<n && s[j]!='0' ){
+                j++;
+                cnt++;
             }
-            else {
-                store.push_back({len, s[i - 1]});
-                len = 1;
+            if(left[i]>0 && right[j-1]>0){
+                ans=max(left[i]+right[j-1],ans);
             }
+            i=j-1;
         }
-        store.push_back({len, s[r]});
-
-        int MaxRange = 0;
-        for (int i = 0; i + 2 < store.size(); i += 2) {
-            int range = store[i].first + store[i + 2].first;
-            MaxRange = max(MaxRange, range);
-        }
-        return ones + MaxRange;
+        return cnt+ans;
     }
 };
