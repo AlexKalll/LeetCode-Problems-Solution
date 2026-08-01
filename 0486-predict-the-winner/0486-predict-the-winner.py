a8c41a947a3299c05f4a6031eed9099545439792
Kaletsidik Ayalew <alexkalalw@gmail.com>
2026-08-01 20:20:12 +0300

    Time: 0 ms (100%), Space: 19.3 MB (56.14%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,28 +1,15 @@
 class Solution:
-    def predictTheWinner(self, nums: List[int]) -> bool:
+    def predictTheWinner(self, nums: list[int]) -> bool:
         n = len(nums)
 
-        def helper(start, end):
-            if start == end:
-                return nums[start]
+        def dp(i, j, memo):
+            if i > j:
+                return 0
 
-            choice1 = nums[start] - helper(start + 1, end)
-            choice2 = nums[end] - helper(start, end - 1)
-
-            return max(choice1, choice2)
-
-        res = helper(0, n-1)
-
-        return res >= 0
-"""
-explanation:
-base case: when jst one unselected num remains, returns it and terminates teh recursion
-choice1: when player1 selects at start, then p2 plays optimally as well
-choice2: when player1 selects at end, then p2 plays optimally as well
-finally, after all the possible case return the max of the two so that player1 will play optimally
-
-return: player1 wins or in a tie
-
-time complexity: O(2^n) since we are doing recursive call for choice1 and choice2
-space: O(n)
-"""
+            if (i, j) not in memo:
+                memo[(i, j)] = max(
+                    nums[i] - dp(i+1, j, memo),
+                    nums[j] - dp(i, j-1, memo)
+                )
+            return memo[(i, j)]
+        return dp(0, n-1, {}) >= 0
